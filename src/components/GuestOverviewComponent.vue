03fef51c7059727be86069ef34356f78677413dd
Luis Ernesto Macias Avila <bootcamp04@tabit-gmbh.de>
2020-03-20 11:27:15 +0100

    #DCM2-12 #DCM2-25 Home page with button + Added vue-router library (official router for vuejs) + Added HeaderComponent (empty) + Added HomePageComponent (empty) + Added GuestOverviewComponent (empty) + Added some basic routes for navigation + Added tabit icon

---
+++ src/components/GuestOverviewComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Guest Overview:</h1>
+</template>
+
+<script>
+export default {
+  name: 'GuestOverviewComponent',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header mb-5 text-center">
+     <h1> <img src="../assets/logo.png" />DCM2</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
