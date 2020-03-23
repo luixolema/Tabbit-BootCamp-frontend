20eeeb3c71888ba9145fba2c0b12ab34a48aec36
alexis <bootcamp03@tabit-gmbh,de>
2020-03-23 21:05:05 +0100

    #DCM2-12 #DCM2-24 first proposal using vuetify

---
+++ src/views/dashboard/pages/Guests.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container
+    id="guests"
+    fluid
+    tag="section"
+  >
+    <h1>guests</h1>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    //
+  }
+</script>
