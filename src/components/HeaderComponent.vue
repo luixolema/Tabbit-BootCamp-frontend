f7c205597c1bbaf40fb978a0e77dc1919b049fe2
Luis Ernesto Macias Avila <bootcamp04@tabit-gmbh.de>
2020-03-27 14:22:11 +0100

    #DCM2-12 #DCM2-44 Bugfixing: fix wrong Label, remove checkbox + Added jest dependencies for testing

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="header mb-5 text-center">
-     <h1> <img src="../assets/logo.png" />DCM2</h1>
+    <h1> <img src="../assets/logo.png">DCM2</h1>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HeaderComponent',
-  components: {
-    
+  export default {
+    name: 'HeaderComponent',
+    components: {
+
+    },
   }
-}
 </script>
 
 <style>
--- src/components/HomePageComponent.vue
+++ src/components/HomePageComponent.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <button link class="btn btn-primary">
-      <router-link to="/guest-overview">Guest Overview</router-link>
+    <button
+      link
+      class="btn btn-primary"
+    >
+      <router-link to="/guest-overview">
+        Guest Overview
+      </router-link>
     </button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HomePageComponent',
-  components: {}
-};
+  export default {
+    name: 'HomePageComponent',
+    components: {},
+  }
 </script>
 
 <style>
