035cf104f630d673ff475c94dd76aebee35c65a5
Luis Ernesto Macias Avila <bootcamp04@tabit-gmbh.de>
2020-04-14 17:25:27 +0200

    #DCM2-16 #DCM2-106 Modal dialog with the three pages template

---
+++ src/views/components/guests/CheckInDialogComponent.vue
@@ -0,0 +1,155 @@
+<template>
+  <v-dialog
+    v-model="open"
+    width="900"
+  >
+    <v-card>
+      <v-card-title class="d-flex flex-row justify-space-between">
+        <span>
+          Check in guest
+        </span>
+        <span
+          class="cursor-pointer"
+          @click="cancel"
+        >X</span>
+      </v-card-title>
+      <v-card-text>
+        <v-stepper
+          v-model="step"
+          non-linear
+        >
+          <v-stepper-header>
+            <v-stepper-step
+              step="1"
+            >
+              Name of step 1
+            </v-stepper-step>
+
+            <v-divider />
+
+            <v-stepper-step
+              step="2"
+            >
+              Name of step 2
+            </v-stepper-step>
+
+            <v-divider />
+
+            <v-stepper-step step="3">
+              Name of step 3
+            </v-stepper-step>
+          </v-stepper-header>
+
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <div class="mb-3">
+                // content 1
+              </div>
+
+              <v-divider />
+              <div class="d-flex flex-row justify-space-between">
+                <v-btn
+                  text
+                  @click="cancel"
+                >
+                  Cancel
+                </v-btn>
+
+                <v-btn
+                  @click="step = 2"
+                >
+                  Next
+                </v-btn>
+              </div>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+              <div class="mb-3">
+                // content 2
+              </div>
+
+              <v-divider />
+              <div class="d-flex flex-row justify-space-between">
+                <v-btn
+                  @click="step = 1"
+                >
+                  Previous
+                </v-btn>
+
+                <v-btn
+                  @click="step = 3"
+                >
+                  Next
+                </v-btn>
+              </div>
+            </v-stepper-content>
+
+            <v-stepper-content step="3">
+              <div class="mb-3">
+                // content 3
+              </div>
+
+              <v-divider />
+              <div class="d-flex flex-row justify-space-between">
+                <v-btn
+                  @click="step = 2"
+                >
+                  Previous
+                </v-btn>
+
+                <v-btn
+                  color="primary"
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </div>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  const CheckinDialogComponent = {
+    props: {
+      open: {
+        type: Boolean,
+        required: true,
+      },
+      onSave: {
+        type: Function,
+      },
+      onCancel: {
+        type: Function,
+      },
+    },
+    data: () => ({
+      stayDto: {},
+      step: 1,
+    }),
+    methods: {
+      cancel () {
+        this.$emit('onCancel')
+      },
+      save () {
+        this.$emit('onSave', this.stayDto)
+      },
+    },
+  }
+  export default CheckinDialogComponent
+</script>
+
+<style scoped>
+  .v-stepper__header{
+    box-shadow: none !important;
+  }
+  .v-stepper{
+    box-shadow: none !important;
+  }
+  .v-stepper__content{
+    padding: 0px 0px 0px 0px;
+  }
+</style>
--- src/views/components/guests/GuestOverviewComponent.vue
+++ src/views/components/guests/GuestOverviewComponent.vue
@@ -59,7 +59,7 @@
               block
               small
               :disabled="!noCheckedinGuestIsSelected"
-              @click="checkInSelectedGuest"
+              @click="toggleOpenCheckinDialog"
             >
               Check In
               <v-icon
@@ -69,6 +69,11 @@
                 mdi-account-check
               </v-icon>
             </v-btn>
+            <checkin-dialog-component
+              :open="openCheckinDialog"
+              @onSave="checkInSelectedGuest"
+              @onCancel="toggleOpenCheckinDialog"
+            />
           </v-col>
         </v-row>
       </v-card-text>
@@ -121,6 +126,7 @@
 <script>
   import GuestService from '@/services/GuestService'
   import NotificationService from '@/services/NotificationService'
+  import CheckinDialogComponent from '@/views/components/guests/CheckinDialogComponent'
 
   const headers = [
     { text: 'Id', value: 'id' },
@@ -132,11 +138,15 @@
 
   export default {
     name: 'GuestOverviewComponent',
+    components: {
+      CheckinDialogComponent,
+    },
     data: () => {
       return {
         guests: [],
         headers,
         filterBYCheckedIn: 'all',
+        openCheckinDialog: false,
       }
     },
     computed: {
@@ -187,20 +197,13 @@
             })
         }
       },
-      checkInSelectedGuest () {
-        // if (!this.noCheckedinGuestIsSelected) {
-        //   NotificationService.info('You should and only select NOT checked in guests')
-        //   return
-        // }
-
-        // GuestService.checkInGuests(this.selectedGuest.id)
-        //   .then(() => {
-        //     NotificationService.sucess('The guest has been cheked-in correctly')
-        //     this.selectedGuest.checkedin = true
-        //     // we need to verify if the checkedin user its correctly shown in the table
-        //   }).catch(error => {
-        //     NotificationService.error(null, error)
-        //   })
+      toggleOpenCheckinDialog () {
+        this.openCheckinDialog = !this.openCheckinDialog
+      },
+      checkInSelectedGuest (stayDto) {
+        console.log('data to save: ', stayDto)
+        this.toggleOpenCheckinDialog()
+        alert('Saved!!')
       },
       selectGuest (guest) {
         this.$store.commit('guestModule/setSelectedGuest', guest)
