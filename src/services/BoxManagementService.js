4c6a6e67e56d63a5f481d4a646b5b64aba468ad6
alexis <bootcamp03@tabit-gmbh,de>
2020-04-22 20:53:00 +0200

    #DCM2-16 #DCM2-122 validation using box management

--- src/services/BoxManagementService.js
+++ src/services/BoxManagementService.js
@@ -4,9 +4,7 @@ const apiUrl = 'http://localhost:8080/api/box'
 
 const BoxManagementService = {
   isBoxEmpty (boxNumber) {
-    return axios.post(apiUrl + '/isFree', boxNumber, {
-      headers: { 'Content-Type': 'application/json' },
-    })
+    return axios.get(apiUrl + '/' + boxNumber + '/isFree')
   },
 }
 
